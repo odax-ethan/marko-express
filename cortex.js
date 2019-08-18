85b2774c7564d37a63f6f1966ea03a4082278469
ethan <eidrory@massart.edu>
2019-08-18 18:59:26 -0400

    intergrated basic Johnny-five (none system class)

--- cortex.js
+++ cortex.js
@@ -1,10 +1,16 @@
 require('marko/node-require');
 
+const ip = require('ip'); // include ip
 const express = require('express');
+var five = require("johnny-five");
+
 const markoPress = require('marko/express'); //enable res.marko
 const lassoWare = require('lasso/middleware');
 const indexTemplate = require('./scr/template/index.marko');
 
+
+const systemIP  = ip.address();
+const hostIP = '0.0.0.0';
 const port = 8080;
 const isProduction = (process.env.NODE_ENV === 'production');
 
@@ -21,21 +27,75 @@ require('lasso').configure({
 });
 
 const app = express();
+const server = require('http').Server(app); // create http server instance through express
 app.use(markoPress());
 app.use(lassoWare.serveStatic());
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+var ledSTATE = null
 
 app.get('/', function (req, res) {
     res.marko(indexTemplate, {
         name: 'Frank',
-        count: 30,
         colors: ['red', 'green', 'blue']
     });
 });
 
-app.listen(port, function () {
-    console.log('Server started! Try it out:\nhttp://localhost:' + port + '/');
-
+app.listen(port, hostIP, function () {
+    console.log('Server started! Try it out:\nhttp://'+ systemIP+':' + port + '/');
     if (process.send) {
         process.send('online');
     }
 });
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
+var board = new five.Board({
+  port: "COM6"
+});
+
+// The board's pins will not be accessible until
+// the board has reported that it is ready
+board.on("ready", function() {
+
+  // this.pinMode(13, this.MODES.OUTPUT);
+  //
+  // this.loop(500, () => {
+  //   // Whatever the last value was, write the opposite
+  //   this.digitalWrite(13, this.pins[13].value ? 0 : 1);
+  // });
+
+  // Create a standard `led` component instance
+  var led = new five.Led(13);
+
+  this.repl.inject({
+   // Allow limited on/off control access to the
+   // Led instance from the REPL.
+   on: function() {
+     led.on();
+     ledSTATE = 1
+   },
+   off: function() {
+     led.off();
+     ledSTATE = 2
+   }
+ });
+
+});
+
+
+
+
+
+// board.on("ready", function() {
+//
+//   // Create a standard `led` component instance
+//   var led = new five.Led(13);
+//
+//   // "blink" the led in 500ms
+//   // on-off phase periods
+//   led.blink(500);
+// });
